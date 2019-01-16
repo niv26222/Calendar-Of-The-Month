6686f29157831e6b1b5627db8b492d16649daee1
Niv Ben Abat <niv.benavat@gmail.com>
2019-01-16 09:33:41 +0200

    Update CalendarOfTheMonth.py

--- CalendarOfTheMonth.py
+++ CalendarOfTheMonth.py
@@ -9,7 +9,7 @@ viewCalendar = lambda yy, mm:print("\n\n Calendar > \n %s\n" % calendar.month(yy
 while True:
         if str(input("[+] Star [Y/N] ? ")).strip().lower() == "y":
                 try:
-                      viewCalendar(int(input(\nYear: ")), int(input("Month: ")))
+                      viewCalendar(int(input("\nYear: ")), int(input("Month: ")))
                 except IndexError:
                         print("   -> Try Again! with vaild numbers!\n")
         else:
